0aa98cf77bfc2a93963f1159b6c5497254642e7f
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-04-02 13:21:03 +0600

    added more components and routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "./components/Root";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+        ]
     },
 ]);
